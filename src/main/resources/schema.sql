32076e28216794e5c4b4049361638fcafe95be82
Kimberly Soler <solerjaramillokimberlypaola@gmail.com>
2024-09-06 18:53:01 -0500

    Se ajusta query ya que hacia falta definir la columna role

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(10)
 );
